a493620f8499c6c8f782ae854fd83b297a23963b
Angel1os <101555514+Angel1os@users.noreply.github.com>
2023-07-21 16:57:09 +0000

    Project setup

---
+++ src/main/java/com/springboot/healthaidservice/HealthAidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.healthaidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAidServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthAidServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/healthaidservice/HealthAidServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.healthaidservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAidServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
